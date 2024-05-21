26c6e5caff9da0588c061823169f4aa29d7851f6
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-05-22 02:31:56 +0300

    kaynakca iletisim added

--- src/components/Iletisim.jsx
+++ src/components/Iletisim.jsx
@@ -2,13 +2,23 @@ import { Link } from "react-router-dom";
 
 export default function Iletisim() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-slate-100">
-      <h1 className="text-4xl font-bold text-slate-900 mb-8">İletişim Sayfası</h1>
-      <Link to="/">
-        <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700">
-          Ana Sayfaya Dön
-        </button>
-      </Link>
-    </div>
+    <div className="flex flex-col  items-center min-h-[200vh]  bg-gradient-to-b from-gray-900 via-purple-700 to-purple-900 ">
+    <ul className="font-bold mt-32 text-transparent bg-clip-text bg-gradient-to-r mb- from-amber-300 via-amber-400 to-amber-500 ">
+      <li>
+        <h1 className="text-4xl font-bold  mb-8">
+        besinilacetkilesimleri@gmail.com
+        </h1>
+      </li>
+      
+      <li>
+
+    <Link to="/">
+      <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700">
+        Ana Sayfaya Dön
+      </button>
+    </Link>
+      </li>
+    </ul>
+  </div>
   );
 }
--- src/components/Kaynakca.jsx
+++ src/components/Kaynakca.jsx
@@ -2,13 +2,54 @@ import { Link } from "react-router-dom";
 
 export default function Kaynakca() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-slate-100">
-      <h1 className="text-4xl font-bold text-slate-900 mb-8">Kaynakça Sayfası</h1>
+    <div className="flex flex-col  items-center min-h-[200vh]  bg-gradient-to-b from-gray-900 via-purple-700 to-purple-900 ">
+      <ul className="font-bold mt-32 text-transparent bg-clip-text bg-gradient-to-r mb- from-amber-300 via-amber-400 to-amber-500 ">
+        <li>
+          <h1 className="text-4xl font-bold  mb-8">
+            Beslenme ve İlaç Etkileşimi
+          </h1>
+        </li>
+        <li>
+          <h1 className="text-4xl font-bold  mb-8">
+            Handbook of Food-Drug Interactions
+          </h1>
+        </li>
+        <li>
+          <h1 className="text-4xl font-bold  mb-8">
+            Handbook of Drug-Nutrient Interactions (Second Edition)
+          </h1>
+        </li>
+        <li>
+          <h1 className="text-4xl font-bold  mb-8">
+            {" "}
+            <a href="https://doi.org/10.1016/B978-0-12-820472-6.00066-9">
+              https://doi.org/10.1016/B978-0-12-820472-6.00066-9
+            </a>
+          </h1>
+        </li>
+        <li>
+          <h1 className="text-4xl font-bold  mb-8">
+            {" "}
+            <a href="https://doi.org/10.1016/B978-0-12-820472-6.00194-8">
+              https://doi.org/10.1016/B978-0-12-820472-6.00194-8
+            </a>
+          </h1>
+        </li>
+        <li>
+          <h1 className="text-4xl font-bold  mb-8">
+            Bushra R, Aslam N, Khan AY. Food-Drug Interactions. Oman Med J 2011
+            March; 26(2):77-83.
+          </h1>
+        </li>
+        <li>
+
       <Link to="/">
         <button className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700">
           Ana Sayfaya Dön
         </button>
       </Link>
+        </li>
+      </ul>
     </div>
   );
 }
